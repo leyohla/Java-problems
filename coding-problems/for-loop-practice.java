02e0a238e5afed81f128a69d0588e2eb96ab4377
Leyla Hunn <47248244+leylahunn@users.noreply.github.com>
2019-04-12 09:20:44 +0100

    Update and rename for-loops to for-loop-practice.java

--- coding-problems/for-loop-practice.java
+++ coding-problems/for-loop-practice.java
@@ -1,3 +1,5 @@
+// For Loop practice program: Find the number of stars by looping the string 'Star' iteratively, and stop at 10.
+
 public class forLoops {
 	
 	public static void main(String[] args) {
@@ -5,11 +7,9 @@ public class forLoops {
 	
 		for(row = 1; row <=10; row++) {
 		for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
-			System.out.println("yo");
+			System.out.println("Star");
 		}
 		System.out.println("Go to next line");
 		}
 	}
 }
-
-//For Loop practice program: Looping 'numberOfStars' iteratively and stop at 10.
